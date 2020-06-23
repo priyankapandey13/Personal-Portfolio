f6ad96dda82b8e105dde27ee478f9b6e01b6706b
Priyanka <Priyanka.mydesire@gmail.com>
2020-06-23 03:21:09 +0200

    Deleted extra code

--- js/main.js
+++ js/main.js
@@ -2,43 +2,14 @@ console.log('Script loaded');
 
 // document.getElementById("navigation-content").style.transform="translate(0px)";
 
-// basicFunctions()
-// function basicFunctions() {
-    
-// }
-
-
-// let headerMenu = document.getElementsByClassName("menubar");
-// headerMenu.addEventListener("click", function(){
-//     console.log("im reached here");
-    
-//     // document.getElementById("navigation-content").style.transform="translate(0px)";
-//   }); 
-
-// document.querySelectorAll(".menubar").addEventListener("click", myFunction())
-
-
-
 window.onload = function () {
 // For loader window need to uncomment it later
     // let loaderwindow = document.querySelector("#loader");
     // loaderwindow.classList.add('loader-load');
 // For loader window need to uncomment it later
 
-    // gsap.to('#loader',1,{y:"-100%"});
-    // gsap.to('#loader',1,{opacity:0});
-    // gsap.to('#loader',0,{display:"none",delay:1});
-    // gsap.to('#header',0,{display:"block",delay:1})
-    // gsap.to('#navigation-content',0,{display:"none"});
-    // gsap.to('#navigation-content',0,{display:"flex",delay:1});
 
-// Autocomplete index content
-
-
-
-// Autocomplete index content
-
-
+// Menu bar options
     let menubarOpen = document.querySelector(".menubar-btn");
     menubarOpen.addEventListener("click", openMenu);
     
@@ -51,13 +22,10 @@ window.onload = function () {
     let contact = document.getElementById("contact");
     let breaker = document.getElementById("breaker");
     
-
-    
     document.getElementById("home-link").addEventListener("click", onlyHome);
     document.getElementById("about-link").addEventListener("click", onlyAbout);
     document.getElementById("portfolio-link").addEventListener("click", onlyPortfolio);
     document.getElementById("contact-link").addEventListener("click", onlyContact);
-
 
     let showHome = document.getElementById("home-link-ss");
     showHome.addEventListener("click", onlyHome);
@@ -65,16 +33,11 @@ window.onload = function () {
     let showAbout = document.getElementById("about-link-ss");
     showAbout.addEventListener("click", onlyAbout);
 
-
-
     let showPortfolio = document.getElementById("portfolio-link-ss");
     showPortfolio.addEventListener("click", onlyPortfolio);
 
-
     let showContact = document.getElementById("contact-link-ss");
     showContact.addEventListener("click", onlyContact);
-
-
 
     function onlyHome() {
         home.style.display="block";
@@ -117,8 +80,6 @@ window.onload = function () {
         breaker.style.display="block";
         closeMenu()
     }
-    
-    
 
     $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
@@ -129,11 +90,7 @@ window.onload = function () {
             $("#navigation-bar").removeClass("scrolling");
         }
     });
-
-
 };
-
-
 
 
 function openMenu(){
@@ -149,30 +106,3 @@ function closeMenu(){
         breaker.style.display="none";
     }, 1200);
 }
-
-
-
-
-
-
-
-
-// function myMap() {
-//     var mapProp= {
-//       center:new google.maps.LatLng(51.508742,-0.120850),
-//       zoom:5,
-//     };
-//     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-//     }
-//     // </script>
-    
-//     <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
-
-
-
-// function submitform(name, email, subject, message){
-   
-// }
-
-
-// name=priyanka&email=priyanka.test%40demo.com&subject=testing123&message=lorem+ipsum#
