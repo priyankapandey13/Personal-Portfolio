7a519920f7fa74d294ff012d6c8f989b24a3dbb9
Priyanka <Priyanka.mydesire@gmail.com>
2020-07-08 00:53:26 +0200

    Updating links and some content

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Personal-Portfolio
 My personal portfolio
+
+It can be seen here for now :
+https://htmlpreview.github.io/?https://github.com/priyankapandey13/Personal-Portfolio/blob/master/index.html#
--- js/main.js
+++ js/main.js
@@ -1,108 +1,111 @@
-console.log('Script loaded');
+console.log("Script loaded");
 
 // document.getElementById("navigation-content").style.transform="translate(0px)";
 
 window.onload = function () {
-// For loader window need to uncomment it later
-    // let loaderwindow = document.querySelector("#loader");
-    // loaderwindow.classList.add('loader-load');
-// For loader window need to uncomment it later
+  // For loader window need to uncomment it later
+  // let loaderwindow = document.querySelector("#loader");
+  // loaderwindow.classList.add('loader-load');
+  // For loader window need to uncomment it later
 
+  // Menu bar options
+  let menubarOpen = document.querySelector(".menubar-btn");
+  menubarOpen.addEventListener("click", openMenu);
 
-// Menu bar options
-    let menubarOpen = document.querySelector(".menubar-btn");
-    menubarOpen.addEventListener("click", openMenu);
-    
-    let menubarClose = document.querySelector(".navigation-close");
-    menubarClose.addEventListener("click", closeMenu);
+  let menubarClose = document.querySelector(".navigation-close");
+  menubarClose.addEventListener("click", closeMenu);
 
-    let home = document.getElementById("header");
-    let about = document.getElementById("about");
-    let portfolio = document.getElementById("portfolio");
-    let contact = document.getElementById("contact");
-    let breaker = document.getElementById("breaker");
-    
-    document.getElementById("home-link").addEventListener("click", onlyHome);
-    document.getElementById("about-link").addEventListener("click", onlyAbout);
-    document.getElementById("portfolio-link").addEventListener("click", onlyPortfolio);
-    document.getElementById("contact-link").addEventListener("click", onlyContact);
+  let home = document.getElementById("header");
+  let about = document.getElementById("about");
+  let portfolio = document.getElementById("portfolio");
+  let contact = document.getElementById("contact");
+  let breaker = document.getElementById("breaker");
 
-    let showHome = document.getElementById("home-link-ss");
-    showHome.addEventListener("click", onlyHome);
+  document.getElementById("home-link").addEventListener("click", onlyHome);
+  document.getElementById("about-link").addEventListener("click", onlyAbout);
+  document
+    .getElementById("portfolio-link")
+    .addEventListener("click", onlyPortfolio);
+  document
+    .getElementById("contact-link")
+    .addEventListener("click", onlyContact);
 
-    let showAbout = document.getElementById("about-link-ss");
-    showAbout.addEventListener("click", onlyAbout);
+  let showHome = document.getElementById("home-link-ss");
+  showHome.addEventListener("click", onlyHome);
 
-    let showPortfolio = document.getElementById("portfolio-link-ss");
-    showPortfolio.addEventListener("click", onlyPortfolio);
+  let showAbout = document.getElementById("about-link-ss");
+  showAbout.addEventListener("click", onlyAbout);
 
-    let showContact = document.getElementById("contact-link-ss");
-    showContact.addEventListener("click", onlyContact);
+  let showPortfolio = document.getElementById("portfolio-link-ss");
+  showPortfolio.addEventListener("click", onlyPortfolio);
 
-    function onlyHome() {
-        home.style.display="block";
-        about.style.display="none";
-        // blog.style.display="none";
-        portfolio.style.display="none";
-        contact.style.display="none";
-        breaker.style.display="block";
-        closeMenu()
+  let showContact = document.getElementById("contact-link-ss");
+  showContact.addEventListener("click", onlyContact);
+
+  function onlyHome() {
+    home.style.display = "block";
+    about.style.display = "none";
+    // blog.style.display="none";
+    portfolio.style.display = "none";
+    contact.style.display = "none";
+    breaker.style.display = "block";
+    closeMenu();
+  }
+
+  function onlyAbout() {
+    home.style.display = "none";
+    about.style.display = "block";
+    // blog.style.display="none";
+    portfolio.style.display = "none";
+    contact.style.display = "none";
+    breaker.style.display = "block";
+    closeMenu();
+  }
+
+  function onlyPortfolio() {
+    console.log("I am here");
+
+    home.style.display = "none";
+    about.style.display = "none";
+    // blog.style.display="none";
+    portfolio.style.display = "block";
+    contact.style.display = "none";
+    breaker.style.display = "block";
+    closeMenu();
+  }
+
+  function onlyContact() {
+    home.style.display = "none";
+    about.style.display = "none";
+    // blog.style.display="none";
+    portfolio.style.display = "none";
+    contact.style.display = "block";
+    breaker.style.display = "block";
+    closeMenu();
+  }
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+      $("#navigation-bar").addClass("scrolling");
+    } else {
+      $("#navigation-bar").removeClass("scrolling");
     }
-    
-    function onlyAbout() {
-        home.style.display="none";
-        about.style.display="block";
-        // blog.style.display="none";
-        portfolio.style.display="none";
-        contact.style.display="none";
-        breaker.style.display="block";
-        closeMenu()
-    }
-
-    function onlyPortfolio() {
-        console.log('I am here');
-        
-        home.style.display="none";
-        about.style.display="none";
-        // blog.style.display="none";
-        portfolio.style.display="block";
-        contact.style.display="none";
-        breaker.style.display="block";
-        closeMenu()
-    }
-    
-    function onlyContact() {
-        home.style.display="none";
-        about.style.display="none";
-        // blog.style.display="none";
-        portfolio.style.display="none";
-        contact.style.display="block";
-        breaker.style.display="block";
-        closeMenu()
-    }
-
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
-    
-        if (scroll >= 50) {
-            $("#navigation-bar").addClass("scrolling");
-        } else {
-            $("#navigation-bar").removeClass("scrolling");
-        }
-    });
+  });
 };
 
-
-function openMenu(){
-    const navigation = document.getElementById("navigation-content");
-    // document.getElementById("navigation-content").style.transform="translate(0px)";
-    navigation.style.transform="translate(0px)";
-navigation.classList.add("mystyle");
+function openMenu() {
+  const navigation = document.getElementById("navigation-content");
+  // document.getElementById("navigation-content").style.transform="translate(0px)";
+  navigation.style.transform = "translate(0px)";
+  navigation.classList.add("mystyle");
 }
-function closeMenu(){
-    document.getElementById("navigation-content").style.transform="translate(0px, -100%)";
-    // breaker.style.display="none";
-    setTimeout(() => {
-        breaker.style.display="none";
-    }, 1200);
+function closeMenu() {
+  document.getElementById("navigation-content").style.transform =
+    "translate(0px, -100%)";
+  // breaker.style.display="none";
+  setTimeout(() => {
+    breaker.style.display = "none";
+  }, 1200);
 }
